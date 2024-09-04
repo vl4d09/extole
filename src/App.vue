6341cd9e4f12f44fb730f8df25697dcc3800512c
vl4d09 <ungureanu.vlad19@gmail.com>
2024-09-04 12:10:31 +0300

    new changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="{ dark: isDarkMode }" class="flex justify-center items-center h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="relative flex flex-col max-w-4xl w-full h-full bg-white dark:bg-gray-800 shadow-lg rounded-lg">
+      <header class="p-4 bg-blue-600 dark:bg-gray-800 text-white rounded-t-lg flex items-center justify-between">
+        <!-- Centered Logo -->
+        <div class="flex-grow flex ">
+          <img src="@/assets/logo.png" alt="Logo" class="h-12">
+        </div>
+        <!-- Dark Mode Toggle Button -->
+        <button @click="toggleDarkMode" class="px-4 py-2 bg-gray-700 dark:bg-gray-500 text-white rounded-lg focus:outline-none">
+          {{ isDarkMode ? 'Light Mode' : 'Dark Mode' }}
+        </button>
+      </header>
+
+      <main class="flex-grow overflow-y-auto dark:bg-gray-900 dark:text-white">
+        <ChatComponent />
+      </main>
+
+      <footer class="p-4 bg-gray-800 dark:bg-gray-700 text-white text-center rounded-b-lg">
+        © 2024 Extole. All rights reserved.
+      </footer>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatComponent from './components/ChatComponent.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ChatComponent,
+  },
+  data() {
+    return {
+      isDarkMode: true, // Set dark mode as the default
+    };
+  },
+  created() {
+    this.applyDarkMode();
+  },
+  methods: {
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+      this.applyDarkMode();
+    },
+    applyDarkMode() {
+      document.documentElement.classList.toggle('dark', this.isDarkMode);
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+/* Additional styles can be added here */
 </style>
